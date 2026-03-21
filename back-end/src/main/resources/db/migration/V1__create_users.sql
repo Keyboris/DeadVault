82b42ca3a902a02ea42b955db70cde4ceac4303e
Keyboris <iluyaretskyi@gmail.com>
2026-03-21 14:07:10 +0000

    Bug fix

--- back-end/src/main/resources/db/migration/V1__create_users.sql
+++ back-end/src/main/resources/db/migration/V1__create_users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid()
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     wallet_address VARCHAR(42) NOT NULL UNIQUE,
     email VARCHAR(255),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
